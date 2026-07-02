638cdd2f995dc7254d1127755a37a414753974d8
Francis Earl Fojas <francisearl.fojas@gmail.com>
2026-07-03 01:28:17 +0800

    feat: Updated styles to footer and usage of ng-icon

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BUILD_INFO } from '../../../core/build-info';
 import { DatePipe } from '@angular/common';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, DatePipe],
+  imports: [RouterLink, DatePipe, NgIcon],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
   readonly currentYear: number = new Date().getFullYear();
   readonly buildInfo = BUILD_INFO;
+  readonly techStack: { name: string; version: string; icon: string }[] = [
+    { name: 'Angular', version: '22', icon: 'diAngularOriginal' },
+    { name: 'TypeScript', version: '6', icon: 'diTypescriptOriginal' },
+    { name: 'Tailwind CSS', version: '4', icon: 'diTailwindcssOriginal' },
+  ];
 }
